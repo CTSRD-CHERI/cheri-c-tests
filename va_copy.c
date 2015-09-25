74fb385e0472d4b132a6616743bb2a58c87fe115
David Chisnall <theraven@FreeBSD.org>
2015-09-25 10:38:30 +0000

    Make the va_copy test actually test something.

--- va_copy.c
+++ va_copy.c
@@ -1,23 +1,21 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include "cheri_c_test.h"
 
 void printstuff(char *fmt, ...)
 {
 	va_list ap, ap2;
 	va_start(ap, fmt);
 	va_copy(ap2, ap);
-	while (1) {
+	assert(__builtin_memcap_length_get((void*)ap) == __builtin_memcap_length_get((void*)ap2));
+	for (int i=1 ; i<9 ; i++)
+	{
 		int x = va_arg(ap, int);
-		printf(fmt, x);
-		x = va_arg(ap2, int);
-		printf(fmt, x);
+		int y = va_arg(ap2, int);
+		assert(y == y);
 	}
 	va_end(ap);
 }
 
-int main(void)
-{
+BEGIN_TEST
 	printstuff("%d\n", 1,2,3,4,5,6,7,8);
-	//printf("hello %s", "world");
-	return 0;
-}
+END_TEST
