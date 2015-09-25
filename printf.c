faf45abc91197d2af338d7d1a8028fee57d6e75e
David Chisnall <theraven@FreeBSD.org>
2015-09-25 10:34:25 +0000

    Fix off-by-one error in printf test.

--- printf.c
+++ printf.c
@@ -5,7 +5,7 @@ BEGIN_TEST
 	char expected[] = "hello world";
 	char out[sizeof(expected)+8];
 	int len = sprintf(out, "hello %s", "world");
-	assert(len == sizeof(expected));
+	assert(len == sizeof(expected)-1);
 	for (int i=0 ; i<sizeof(expected) ; i++)
 	{
 		assert(expected[i] == out[i]);
